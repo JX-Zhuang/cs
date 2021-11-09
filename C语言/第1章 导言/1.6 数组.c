bfd341b985702f953e46d51bf5848336ec980e64
JX-Zhuang <who.zhuang@gmail.com>
2021-11-09 23:26:24 +0800

    update

--- C语言/第1章 导言/1.6 数组.c
+++ C语言/第1章 导言/1.6 数组.c
@@ -60,12 +60,117 @@ main2()
 #define MAXWORD 11
 #define IN 1
 #define OUT 0
-practice113()
+practice113Horizontal()
 {
     int c, i, nc, state;
     int len;
     int maxvalue;
     int ovflow;
     int wl[MAXWORD];
-    
+    state = OUT;
+    nc = 0;
+    ovflow = 0;
+    for (i = 0; i < MAXWORD; ++i)
+        wl[i] = 0;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            state = OUT;
+            if (nc > 0)
+                if (nc < MAXWORD)
+                    ++wl[nc];
+                else
+                    ++ovflow;
+            nc = 0;
+        }
+        else if (state == OUT)
+        {
+            state = IN;
+            nc = 1;
+        }
+        else
+            ++nc;
+    }
+    maxvalue = 0;
+    for (i = 1; i < MAXWORD; ++i)
+        if (wl[i] > maxvalue)
+            maxvalue = wl[i];
+    for (i = 1; i < MAXWORD; ++i)
+    {
+        printf("%5d - %5d : ", i, wl[i]);
+        if (wl[i] > 0)
+        {
+            if ((len = wl[i] * MAXHIST / maxvalue) <= 0)
+                len = 1;
+        }
+        else
+            len = 0;
+        while (len > 0)
+        {
+            putchar('*');
+            --len;
+        }
+        putchar('\n');
+    }
+    if (ovflow > 0)
+        printf("There are %d words >= %d\n", ovflow, MAXWORD);
+}
+practice113Vertical()
+{
+    int c, nc, i, state;
+    int maxvalue, ovflow, len;
+    int wl[MAXWORD];
+    len = nc = ovflow = 0;
+    state = OUT;
+    for (i = 1; i < MAXWORD; i++)
+        wl[i] = 0;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n' || c == ' ' || c == '\t')
+        {
+            if (state == IN)
+            {
+                if (nc < MAXWORD)
+                    ++wl[nc];
+                else
+                    ++ovflow;
+            }
+            nc = 0;
+        }
+        else if (state == IN)
+            ++nc;
+        else
+        {
+            state = IN;
+            nc = 1;
+        }
+    }
+    maxvalue = 0;
+    for (i = 1; i < MAXWORD; ++i)
+        if (wl[i] > maxvalue)
+            maxvalue = wl[i];
+    for (i = 1; i < MAXWORD; ++i)
+    {
+        printf("%5d - %5d : ", i, wl[i]);
+        if (wl[i] > 0)
+        {
+            if ((len = wl[i] * MAXHIST / maxvalue) <= 0)
+                len = 1;
+        }
+        else
+            len = 0;
+        while (len > 0)
+        {
+            putchar('*');
+            --len;
+        }
+        putchar('\n');
+    }
+    if (ovflow > 0)
+        printf("There are %d words >= %d\n", ovflow, MAXWORD);
+}
+main()
+{
+    practice113Vertical();
 }
