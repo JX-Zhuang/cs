e0ebea14d0e8e458fb594c3030459a74c3287e7e
JX-Zhuang <who.zhuang@gmail.com>
2021-11-24 13:32:36 +0800

    update

---
+++ C语言/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s1[] = "abc";
+    char s2[] = "abc ";
+    printf("%d\n",strcmp(s1,s2));
+    printf("%ld\n",s1-s2);
+    return 0;
+}
