96b040a27e00c454d0ab9234b687fba519ec0c23
JX-Zhuang <who.zhuang@gmail.com>
2021-12-15 13:32:53 +0800

    7-8

---
+++ C语言/《C语言程序设计（第4版）》/练习7-8 方阵循环右移.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int m, n;
+    scanf("%d%d", &m, &n);
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[i][(j + m) % n]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
