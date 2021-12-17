827854dcae73b6456411318c5c1cd8c9d6f51eff
JX-Zhuang <who.zhuang@gmail.com>
2021-12-17 13:45:54 +0800

    7-3

---
+++ C语言/《C语言程序设计（第4版）》/习题7-3 判断上三角矩阵.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int t,n;
+    scanf("%d",&t);
+    for(int i = 1;i<=t;i++){
+        scanf("%d",&n);
+        for(int j = 0;j<n;j++){
+            for(int k = 0;k<n;k++){
+                
+            }
+        }
+    }
+    return 0;
+}
