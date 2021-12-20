1415192b5b8f6858635c61ca0f5c5efefbbc51b7
JX-Zhuang <who.zhuang@gmail.com>
2021-12-20 13:34:19 +0800

    7-3

--- C语言/《C语言程序设计（第4版）》/习题7-3 判断上三角矩阵.c
+++ C语言/《C语言程序设计（第4版）》/习题7-3 判断上三角矩阵.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
-int main(){
-    int t,n;
-    scanf("%d",&t);
-    for(int i = 1;i<=t;i++){
-        scanf("%d",&n);
-        for(int j = 0;j<n;j++){
-            for(int k = 0;k<n;k++){
-                
+int main()
+{
+    int t, n, yes = 1, num;
+    scanf("%d", &t);
+    int arr[t];
+    for (int i = 1; i <= t; i++)
+    {
+        arr[i-1] = 1;
+        scanf("%d", &n);
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                scanf("%d", &num);
+                if (k < j && num != 0)
+                {
+                    arr[i-1] = 0;
+                }
             }
+        }
+    }
+    for(int i = 0;i<t;i++){
+        if (arr[i])
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
         }
     }
     return 0;
