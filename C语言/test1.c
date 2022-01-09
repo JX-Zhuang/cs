568f8b93618d136ca23d072ce41d86b377cf91a4
JX-Zhuang <who.zhuang@gmail.com>
2022-01-09 23:27:58 +0800

    update

--- C语言/test1.c
+++ C语言/test1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main()
+int *f(int *p)
 {
-    // int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    return p;
+}
+int main(int argc, char **argv)
+{
+    // int arr[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     // int m = sizeof(arr) / sizeof(arr[0]),
     //     n = sizeof(arr[0]) / sizeof(arr[0][0]);
     // for (int i = 0; i < m; i++)
@@ -14,20 +18,41 @@ int main()
     // float f = 1.0;
     // short c = *(short *)&f;
     // printf("%d\n", c);
+
     //5
-    int x[] = {1, 2, 3}, *p = x, y;
-    for (int i = 0; i < sizeof(x) / sizeof(x[0]); i++)
+    // int x[] = {1, 2, 3}, *p = x, y;
+    // for (int i = 0; i < sizeof(x) / sizeof(x[0]); i++)
+    // {
+    //     printf("x[%d] = %d,%p\n", i, x[i], &x[i]);
+    // }
+    // printf("p = %p,*p = %d,&p = %p\n", p, *p, &p);
+    // y = *p++;
+    // printf("p = %p,*p = %d,&p = %p\n", p, *p, &p);
+    // printf("%d\n", y);
+    // printf("%d\n", *p);
+    // for (int i = 0; i < sizeof(x) / sizeof(x[0]); i++)
+    // {
+    //     printf("x[%d] = %d,%p\n", i, x[i], &x[i]);
+    // }
+
+    //15
+    // struct
+    // {
+    //     int k;
+    //     char s[30];
+    // } a[5] = {{1, "ABC"}, {2, "abc"}}, *p = a;
+    // // printf("%c\n", *p->s);
+    // printf("%c\n", *(p++)->s);
+
+    // printf("%c\n", **argv);
+    for (int i = 0; i < argc; i++)
     {
-        printf("x[%d] = %d,%p\n", i, x[i], &x[i]);
+        // printf("%s\n", argv[i]);
     }
-    printf("p = %p,*p = %d,&p = %p\n", p, *p, &p);
-    y = *p++;
-    printf("p = %p,*p = %d,&p = %p\n", p, *p, &p);
-    printf("%d\n", y);
-    printf("%d\n", *p);
-    for (int i = 0; i < sizeof(x) / sizeof(x[0]); i++)
+    while (**argv)
     {
-        printf("x[%d] = %d,%p\n", i, x[i], &x[i]);
+        printf("%c\n", **argv++);
     }
+    // printf("%s", *argv);
     return 0;
 }
