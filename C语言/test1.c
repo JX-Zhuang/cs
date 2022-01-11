8506096747290b2368a3dca42df1f91a36965d4d
JX-Zhuang <who.zhuang@gmail.com>
2022-01-12 00:23:32 +0800

    update

--- C语言/test1.c
+++ C语言/test1.c
@@ -5,8 +5,12 @@ void strcopy(char *s, char *t);
 void printStr(char *s);
 int main()
 {
+    int a = 1;
     char *str1 = "hello world!";
     char *str2 = (char *)malloc(strlen(str1) + 1);
+    printf("a    = %p\n", &a);
+    printf("str1 = %p\n", &str1);
+    printf("str2 = %p\n", &str2);
     strcopy(str2, str1);
     printStr(str2);
     return 0;
