365deac0210a5ad1ee9b4a2bde4861787f9fc97b
JX-Zhuang <who.zhuang@gmail.com>
2022-01-13 23:34:27 +0800

    update

--- C语言/第2章 类型、运算符与表达式/2.7 类型转化.c
+++ C语言/第2章 类型、运算符与表达式/2.7 类型转化.c
@@ -2,11 +2,37 @@
 int htoi(char s[]);
 int main()
 {
-    char s[] = "0x123AF";
-    int a = htoi("0x");
-    printf("%s = %d", s, a);
+    printf("%s = %d\n", "0x123AF", htoi("0x123AF"));
+    printf("%s = %d\n", "0x123", htoi("0x123"));
+    printf("%s = %d\n", "0xF", htoi("0xF"));
+    printf("%s = %d\n", "123", htoi("123"));
+    printf("%s = %d\n", "0", htoi("0"));
 }
 int htoi(char s[])
 {
-    
+    int i = 0, n = 0, temp;
+    if (s[i] == '0')
+    {
+        i++;
+        if (s[i] == 'x' || s[i] == 'X')
+            i++;
+    }
+    while (s[i] != '\0')
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            temp = s[i] - '0';
+        }
+        else if (s[i] >= 'a' && s[i] <= 'f')
+        {
+            temp = s[i] - 'a' + 10;
+        }
+        else if (s[i] >= 'A' && s[i] <= 'F')
+        {
+            temp = s[i] - 'A' + 10;
+        }
+        n = temp + n * 16;
+        i++;
+    }
+    return n;
 }
