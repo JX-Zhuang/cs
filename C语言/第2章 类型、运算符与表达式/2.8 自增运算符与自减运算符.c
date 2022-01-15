7cd53606f09b70af8264b9f10b6753f367aadcb5
JX-Zhuang <who.zhuang@gmail.com>
2022-01-15 22:57:18 +0800

    2.8

---
+++ C语言/第2章 类型、运算符与表达式/2.8 自增运算符与自减运算符.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+// 2-4
+// int any(char s[], char c);
+// void squeeze(char s1[], char s2[]);
+// int main()
+// {
+//     {
+//         char s1[] = "";
+//         char s2[] = "a";
+//         squeeze(s1, s2);
+//         printf("s1 = %s\n", s1);
+//         printf("s2 = %s\n", s2);
+//     }
+//     {
+//         char s1[] = "abcadef";
+//         char s2[] = "a";
+//         squeeze(s1, s2);
+//         printf("s1 = %s\n", s1);
+//         printf("s2 = %s\n", s2);
+//     }
+//     {
+//         char s1[] = "aa";
+//         char s2[] = "a";
+//         squeeze(s1, s2);
+//         printf("s1 = %s\n", s1);
+//         printf("s2 = %s\n", s2);
+//     }
+//     {
+//         char s1[] = "abcd";
+//         char s2[] = "";
+//         squeeze(s1, s2);
+//         printf("s1 = %s\n", s1);
+//         printf("s2 = %s\n", s2);
+//     }
+// }
+// int any(char s[], char c)
+// {
+//     int i = 0;
+//     while (s[i] != '\0')
+//     {
+//         if (s[i++] == c)
+//             return i;
+//     }
+
+//     return -1;
+// }
+// void squeeze(char s1[], char s2[])
+// {
+//     int i, j;
+//     for (i = j = 0; s1[i] != '\0'; i++)
+//     {
+//         if (any(s2, s1[i]) == -1)
+//         {
+//             s1[j++] = s1[i];
+//         }
+//     }
+//     s1[j] = '\0';
+// }
