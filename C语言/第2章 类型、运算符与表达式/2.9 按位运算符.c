3d6f20ef4727d43b089c40d0b5a120e90a114eb0
JX-Zhuang <who.zhuang@gmail.com>
2022-01-18 00:10:56 +0800

    update

--- C语言/第2章 类型、运算符与表达式/2.9 按位运算符.c
+++ C语言/第2章 类型、运算符与表达式/2.9 按位运算符.c
@@ -13,6 +13,32 @@ unsigned invert(unsigned x, int p, int n)
     // return (x ^ (bits << (p + 1 - n)) ^ (0)) | (~(bits | (~0 << n)) << (p + 1 - n));
     return x ^ (~(~0 << n) << (p + 1 - n));
 }
+int wordlength()
+{
+    int i;
+    unsigned v = (unsigned)~0;
+    for (i = 1; (v = v >> 1) > 0; i++)
+        ;
+    return i;
+}
+unsigned rightrot(unsigned x, int n)
+{
+    if ((n = n % wordlength()) > 0)
+    {
+        int bits = x & (~(~0 << n));
+        return (x >> n) | (bits << (wordlength() - n));
+    }
+    return x;
+    
+    // int rbit;
+    // while (n-- > 0)
+    // {
+    //     rbit = (x & 1) << (wordlength() - 1);
+    //     x = x >> 1;
+    //     x = x | rbit;
+    // }
+    // return x;
+}
 int main()
 {
     int x = 0b10010101;
@@ -20,4 +46,5 @@ int main()
     printf("%u\n", getbits(x, 4, 3));
     printf("%u\n", setbits(x, 4, 3, y));
     printf("%u\n", invert(x, 4, 3));
+    printf("%u\n", rightrot(y, 3));
 }
