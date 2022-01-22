fc8fec95d2ee399f015a8a1c2e86f34b924d6691
JX-Zhuang <who.zhuang@gmail.com>
2022-01-22 23:41:19 +0800

    update

--- C语言/第4章 函数与程序结构/4.2.c
+++ C语言/第4章 函数与程序结构/4.2.c
@@ -6,17 +6,17 @@ double atof(char s[])
     int i, sign, exp;
     for (i = 0; isspace(s[i]); i++)
         ;
-    sign = (s[i] == '-') ? -1 : 1;
-    if (s[i] == '+' || s[i] == '-')
+    sign = s[i] == '-' ? -1 : 1;
+    if (s[i] == '-' || s[i] == '+')
         i++;
     for (val = 0.0; isdigit(s[i]); i++)
-        val = 10.0 * val + (s[i] - '0');
+        val = val * 10 + (s[i] - '0');
     if (s[i] == '.')
         i++;
     for (power = 1.0; isdigit(s[i]); i++)
     {
-        val = 10.0 * val + (s[i] - '0');
-        power *= 10.0;
+        power *= 10;
+        val = val * 10 + (s[i] - '0');
     }
     val = sign * val / power;
     if (s[i] == 'e' || s[i] == 'E')
