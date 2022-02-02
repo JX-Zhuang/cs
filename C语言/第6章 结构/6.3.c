9b26aa743ff8917c44f40a159d26a891b32d229d
JX-Zhuang <who.zhuang@gmail.com>
2022-02-02 23:39:23 +0800

    update

--- C语言/第6章 结构/6.3.c
+++ C语言/第6章 结构/6.3.c
@@ -1,17 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+#define NKEYS 4
+struct key
+{
+    char *word;
+    int count;
+} keytab[] = {
+    "auto",
+    0,
+    "break",
+    0,
+    "void",
+    0,
+    "while",
+    0};
+int binsearch(char *word, struct key tab[], int n)
+{
+    int low = 0, high = n - 1, mid, cond;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if ((cond = strcmp(word, tab[mid].word)) < 0)
+            high = mid - 1;
+        else if (cond > 0)
+            low = mid + 1;
+        else
+            return mid;
+    }
+    return -1;
+}
 int main()
 {
-    struct
+    int index;
+    char *test[] = {"auto", "void", "void", "while", "hello"};
+    for (int i = 0; i < 5; i++)
     {
-        int len;
-        char *str;
-    } * p;
-    char str = 'A';
-    p->len = 123;
-    p->str = &str;
-    printf("*p->str = %c\n", *p->str);
-    printf("*(p->str) = %c\n", *(p->str));
-    printf("*(*p).str = %c\n", *(*p).str);
-
+        if ((index = binsearch(test[i], keytab, NKEYS)) >= 0)
+        {
+            keytab[index].count++;
+        }
+    }
+    for (int i = 0; i < NKEYS; i++)
+    {
+        printf("word:%s,count:%d\n", keytab[i].word, keytab[i].count);
+    }
     return 0;
 }
