dd2a11bc8886937737725ded0df08b9e702d812c
JX-Zhuang <who.zhuang@gmail.com>
2022-02-04 23:23:58 +0800

    6-5

--- C语言/第6章 结构/6.5.c
+++ C语言/第6章 结构/6.5.c
@@ -9,3 +9,47 @@ struct tnode
     struct tnode *left;
     struct tnode *right;
 };
+void printTNode(struct tnode *node)
+{
+    if (!node)
+        return;
+    printTNode(node->left);
+    printf("word:%s,count:%d\n", node->word, node->count);
+    printTNode(node->right);
+}
+struct tnode *createNode()
+{
+    return malloc(sizeof(struct tnode));
+}
+struct tnode *addNode(struct tnode *node, char *word)
+{
+    if (!node)
+    {
+        struct tnode *newNode = createNode();
+        newNode->word = word;
+        newNode->count = 1;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        return newNode;
+    }
+    int c = strcmp(node->word, word);
+    if (c > 0)
+        node->right = addNode(node->right, word);
+    else if (c < 0)
+        node->left = addNode(node->left, word);
+    else
+        node->count++;
+    return node;
+}
+int main()
+{
+    char *test[] = {"auto", "void", "void", "while", "hello", "while", "break"};
+    char *p = test;
+    struct tnode *root;
+    while (p)
+    {
+        addNode(root, p++);
+    }
+    printTNode(root);
+    return 0;
+}
