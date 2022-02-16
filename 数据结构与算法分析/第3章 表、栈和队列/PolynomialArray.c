bb7560e8dd39b318f0bcd8443e82008a500cac13
JX-Zhuang <who.zhuang@gmail.com>
2022-02-16 23:11:23 +0800

    polynomial

--- 数据结构与算法分析/第3章 表、栈和队列/PolynomialArray.c
+++ 数据结构与算法分析/第3章 表、栈和队列/PolynomialArray.c
@@ -1,13 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MaxDegree 1000
-typedef struct
+struct P
 {
     int CoeffArray[MaxDegree + 1];
     int HighPower;
-} * Polynomial;
+};
+typedef struct P *Polynomial;
+int Max(int a, int b)
+{
+    return a > b ? a : b;
+}
 void ZeroPolynomial(Polynomial Poly)
 {
     for (int i = 0; i <= MaxDegree; i++)
         Poly->CoeffArray[i] = 0;
     Poly->HighPower = 0;
+}
+void AddPolynomial(const Polynomial P1, const Polynomial P2, Polynomial PolySum)
+{
+    int i;
+    ZeroPolynomial(PolySum);
+    i = PolySum->HighPower = Max(P1->HighPower, P2->HighPower);
+    for (; i >= 0; i--)
+    {
+        PolySum->CoeffArray[i] = P1->CoeffArray[i] + P2->CoeffArray[i];
+    }
+}
+void PrintPolynomial(Polynomial P)
+{
+    int i = P->HighPower;
+    for (; i >= 0; i--)
+    {
+        if (P->CoeffArray[i] == 0)
+            continue;
+        if (i != P->HighPower)
+        {
+            printf(P->CoeffArray[i] > 0 ? "+" : "-");
+        }
+        printf("%d", P->CoeffArray[i]);
+        if (i != 0)
+            printf("*x^%d", i);
+    }
+    printf("\n");
+}
+//P1:5*x^4+x^3+x+12
+//P2:3*x^5+2*x^3+x^2
+int main()
+{
+    Polynomial P1 = malloc(sizeof(struct P));
+    ZeroPolynomial(P1);
+    P1->HighPower = 4;
+    P1->CoeffArray[4] = 5;
+    P1->CoeffArray[3] = 1;
+    P1->CoeffArray[1] = 1;
+    P1->CoeffArray[0] = 12;
+    Polynomial P2 = malloc(sizeof(struct P));
+    ZeroPolynomial(P2);
+    P2->HighPower = 5;
+    P2->CoeffArray[5] = 3;
+    P2->CoeffArray[3] = 2;
+    P2->CoeffArray[2] = 1;
+    Polynomial PSum = malloc(sizeof(struct P));
+    AddPolynomial(P1, P2, PSum);
+    PrintPolynomial(PSum);
+    return 0;
 }
