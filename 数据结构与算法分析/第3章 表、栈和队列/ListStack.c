d942ea0173761a18c7d14ec4ab8d0d35baa51763
JX-Zhuang <who.zhuang@gmail.com>
2022-02-18 23:26:12 +0800

    stack list

--- 数据结构与算法分析/第3章 表、栈和队列/ListStack.c
+++ 数据结构与算法分析/第3章 表、栈和队列/ListStack.c
@@ -1,6 +1,69 @@
 #include "Stack.h"
+#include <stdio.h>
+#include <stdlib.h>
 struct Node
 {
     ElementType Element;
     PtrToNode Next;
 };
+int IsEmpty(Stack S)
+{
+    return S->Next == NULL;
+}
+Stack CreateStack(void)
+{
+    Stack S = malloc(sizeof(struct Node));
+    if (S == NULL)
+    {
+        //error
+    }
+    S->Next = NULL;
+    MakeEmpty(S);
+    return S;
+}
+void MakeEmpty(Stack S)
+{
+    if (S == NULL)
+    {
+        //
+    }
+    else
+    {
+        while (!IsEmpty(S))
+            pop(S);
+    }
+}
+void Push(ElementType X, Stack S)
+{
+    PtrToNode Tmp = malloc(sizeof(struct Node));
+    if (Tmp == NULL)
+    {
+        //error
+    }
+    Tmp->Element = X;
+    Tmp->Next = S->Next;
+    S->Next = Tmp;
+}
+ElementType Top(Stack S)
+{
+    if (!IsEmpty(S))
+    {
+        return S->Next->Element;
+    }
+    //error
+    return 0;
+}
+void Pop(Stack S)
+{
+    PtrToNode Tmp;
+    if (IsEmpty(S))
+    {
+        //error
+    }
+    else
+    {
+        Tmp = S->Next;
+        S->Next = S->Next->Next;
+        free(Tmp);
+    }
+}
