065eef770732829b48582bf30975df908f207410
JX-Zhuang <who.zhuang@gmail.com>
2022-02-21 23:09:39 +0800

    Array Queue

---
+++ 数据结构与算法分析/第3章 表、栈和队列/ArrayQueue.c
@@ -0,0 +1,44 @@
+#include "ArrayQueue.h"
+#define MinQueueSize (5)
+struct QueueRecord
+{
+    int Capacity;
+    int Front;
+    int Rear;
+    int Size;
+    ElementType *Array;
+};
+
+int IsEmpty(Queue Q)
+{
+    return Q->Size == 0;
+}
+int IsFull(Queue Q)
+{
+    return Q->Size == Q->Capacity;
+}
+void MakeEmpty(Queue Q)
+{
+    Q->Size = 0;
+    Q->Front = 1;
+    Q->Rear = 0;
+}
+static int Succ(int Value, Queue Q)
+{
+    if (++Value == Q->Capacity)
+        Value = 0;
+    return Value;
+}
+void Enqueue(ElementType X, Queue Q)
+{
+    if (IsFull(Q))
+    {
+        //error
+    }
+    else
+    {
+        Q->Size++;
+        Q->Rear = Succ(Q->Rear, Q);
+        Q->Array[Q->Rear] = X;
+    }
+}
