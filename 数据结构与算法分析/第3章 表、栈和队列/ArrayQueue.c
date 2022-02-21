79b8697e439803f8fc91ba1d76603c85a5c55950
JX-Zhuang <who.zhuang@gmail.com>
2022-02-21 23:51:27 +0800

    Array Queue

--- 数据结构与算法分析/第3章 表、栈和队列/ArrayQueue.c
+++ 数据结构与算法分析/第3章 表、栈和队列/ArrayQueue.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "ArrayQueue.h"
 #define MinQueueSize (5)
 struct QueueRecord
@@ -8,7 +9,12 @@ struct QueueRecord
     int Size;
     ElementType *Array;
 };
-
+static int Succ(int Value, Queue Q)
+{
+    if (++Value == Q->Capacity)
+        Value = 0;
+    return Value;
+}
 int IsEmpty(Queue Q)
 {
     return Q->Size == 0;
@@ -17,17 +23,22 @@ int IsFull(Queue Q)
 {
     return Q->Size == Q->Capacity;
 }
+Queue CreateQueue(int MaxElements)
+{
+    Queue Q = malloc(sizeof(struct QueueRecord));
+    if (Q == NULL)
+    {
+        //error
+    }
+    Q->Capacity = MinQueueSize;
+    MakeEmpty(Q);
+    return Q;
+}
 void MakeEmpty(Queue Q)
 {
     Q->Size = 0;
     Q->Front = 1;
     Q->Rear = 0;
-}
-static int Succ(int Value, Queue Q)
-{
-    if (++Value == Q->Capacity)
-        Value = 0;
-    return Value;
 }
 void Enqueue(ElementType X, Queue Q)
 {
@@ -41,4 +52,36 @@ void Enqueue(ElementType X, Queue Q)
         Q->Rear = Succ(Q->Rear, Q);
         Q->Array[Q->Rear] = X;
     }
+}
+ElementType Front(Queue Q)
+{
+    if (IsEmpty(Q))
+    {
+        //error
+        return 0;
+    }
+    return Q->Array[Q->Front];
+}
+void Dequeue(Queue Q)
+{
+    if (IsEmpty(Q))
+    {
+        //error
+    }
+    else
+    {
+        Q->Size--;
+        Q->Front = Succ(Q->Front, Q);
+    }
+}
+ElementType FrontAndDequeue(Queue Q)
+{
+    if (IsEmpty(Q))
+    {
+        //error
+        return 0;
+    }
+    ElementType Element = Front(Q);
+    Dequeue(Q);
+    return Element;
 }
