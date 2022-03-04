26d05a5e105583235927776b084a3e8db7819218
JX-Zhuang <who.zhuang@gmail.com>
2022-03-04 23:40:54 +0800

    avl

--- 数据结构与算法分析/第4章 树/Avl.h
+++ 数据结构与算法分析/第4章 树/Avl.h
@@ -3,7 +3,14 @@
 struct AvlNode;
 typedef struct AvlNode *Postion;
 typedef struct AvlNode *AvlTree;
+typedef int ElementType;
 
 AvlTree MakeEmpty(AvlTree T);
+Postion Find(ElementType X, AvlTree T);
+Postion FindMin(AvlTree T);
+Postion FindMax(AvlTree T);
+AvlTree Insert(ElementType X, AvlTree T);
+AvlTree Delete(ElementType X, AvlTree T);
+ElementType Retrieve(Postion P);
 
 #endif /* _AvlTree_H */
