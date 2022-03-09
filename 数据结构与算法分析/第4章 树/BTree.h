7375ae973a1f43671f63a8d6b9bdb7789ca34f48
JX-Zhuang <who.zhuang@gmail.com>
2022-03-09 23:39:39 +0800

    BTree

--- 数据结构与算法分析/第4章 树/BTree.h
+++ 数据结构与算法分析/第4章 树/BTree.h
@@ -1,3 +1,13 @@
 #ifndef _BTree_H
 
+struct BTreeNode;
+typedef struct BTreeNode *Position;
+typedef struct BTreeNode *BTree;
+typedef int ElementType;
+
+BTree MakeEmpty(BTree T);
+BTree Insert(ElementType X, BTree T);
+BTree Delete(ElementType X, BTree T);
+Position Find(ElementType X, BTree T);
+
 #endif /* _BTree_H */
