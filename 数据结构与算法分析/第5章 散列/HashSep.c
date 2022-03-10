6b03eed9a6682beaf5969092e21f4ce2f5559285
JX-Zhuang <who.zhuang@gmail.com>
2022-03-10 23:46:51 +0800

    拉链法

--- 数据结构与算法分析/第5章 散列/HashSep.c
+++ 数据结构与算法分析/第5章 散列/HashSep.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "HashSep.h"
 
 struct ListNode
@@ -13,3 +15,31 @@ struct HashTbl
     int TableSize;
     List *TheLists;
 };
+HashTable InitializeTable(int TableSize)
+{
+    HashTable H;
+    int i;
+    H = malloc(sizeof(struct HashTbl));
+    if (H == NULL)
+    {
+        //error;
+    }
+    H->TableSize = TableSize; //prime素数
+    H->TheLists = malloc(sizeof(List) * H->TableSize);
+    if (H->TheLists == NULL)
+    {
+        //error
+    }
+    for (i; i < H->TableSize; i++)
+    {
+        H->TheLists[i] = malloc(sizeof(struct ListNode));
+        if (H->TheLists[i] == NULL)
+        {
+            //error
+        }
+        else
+        {
+            H->TheLists[i]->Next = NULL;
+        }
+    }
+}
