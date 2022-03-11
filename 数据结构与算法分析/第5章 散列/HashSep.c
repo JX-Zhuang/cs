219d3031aa3445576d30d8a0183f60c3a4fb3050
JX-Zhuang <who.zhuang@gmail.com>
2022-03-11 23:42:31 +0800

    update

--- 数据结构与算法分析/第5章 散列/HashSep.c
+++ 数据结构与算法分析/第5章 散列/HashSep.c
@@ -15,6 +15,15 @@ struct HashTbl
     int TableSize;
     List *TheLists;
 };
+int Hash(const char *Key, int TableSize)
+{
+    unsigned int HashVal = 0;
+    while (*Key != '\0')
+    {
+        HashVal = (HashVal << 5) + *Key++;
+    }
+    return HashVal % TableSize;
+}
 HashTable InitializeTable(int TableSize)
 {
     HashTable H;
@@ -43,6 +52,34 @@ HashTable InitializeTable(int TableSize)
         }
     }
 }
-Position Find(ElementType Key, HashTable H){
-    
+Position Find(ElementType Key, HashTable H)
+{
+    Position P;
+    List L;
+    L = H->TheLists[Hash(Key, H->TableSize)];
+    P = L->Next;
+    while (P != NULL && P->Element != Key)
+        P = P->Next;
+    return P;
+}
+void Insert(ElementType Key, HashTable H)
+{
+    Position P, NewCell;
+    List L;
+    P = Find(Key, H);
+    if (P == NULL)
+    {
+        NewCell = malloc(sizeof(struct ListNode));
+        if (NewCell == NULL)
+        {
+            //
+        }
+        else
+        {
+            L = H->TheLists[Hash(Key, H->TableSize)];
+            NewCell->Next = L->Next;
+            NewCell->Element = Key;
+            L->Next = NewCell;
+        }
+    }
 }
