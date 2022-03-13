0b4f9cc52d3b8bb499d766bc533b5d88f3629efe
JX-Zhuang <who.zhuang@gmail.com>
2022-03-13 23:51:07 +0800

    priority queue

--- 数据结构与算法分析/第6章 优先队列(堆)/BinHeap.c
+++ 数据结构与算法分析/第6章 优先队列(堆)/BinHeap.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "BinHeap.h"
 
-#define MinData -100
+#define MinData -100000
 struct HeapStruct
 {
     int Capacity;
@@ -26,4 +26,52 @@ PriorityQueue Initialize(int MaxElements)
     H->Capacity = MaxElements;
     H->Elements[0] = MinData;
     return H;
+}
+
+int IsFull(PriorityQueue H)
+{
+    return H->Size == H->Capacity;
+}
+void Insert(ElementType X, PriorityQueue H)
+{
+    if (IsFull(H))
+    {
+        //error;
+        return;
+    }
+    int i;
+    for (i = ++H->Size; H->Elements[i / 2] > X; i /= 2)
+        H->Elements[i] = H->Elements[i / 2];
+    H->Elements[i] = X;
+}
+int IsEmpty(PriorityQueue H)
+{
+    return H->Size == 0;
+}
+ElementType DeleteMin(PriorityQueue H)
+{
+    if (IsEmpty(H))
+    {
+        //error;
+        return H->Elements[0];
+    }
+    int i, Child;
+    ElementType MinElement = H->Elements[1],
+                LastElement = H->Elements[H->Size--];
+    for (i = 1; 2 * i <= H->Size; i = Child)
+    {
+        Child = i * 2;
+        if (Child < H->Size && H->Elements[Child + 1] < H->Elements[Child])
+            Child++;
+        if (LastElement > H->Elements[Child])
+        {
+            H->Elements[i] = H->Elements[Child];
+        }
+        else
+        {
+            break;
+        }
+    }
+    H->Elements[i] = LastElement;
+    return MinElement;
 }
