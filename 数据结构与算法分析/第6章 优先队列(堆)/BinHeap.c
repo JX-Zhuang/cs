fcbf310a81a68a98d28cfb021588020846ae760b
JX-Zhuang <who.zhuang@gmail.com>
2022-03-13 23:11:54 +0800

    priority queue

--- 数据结构与算法分析/第6章 优先队列(堆)/BinHeap.c
+++ 数据结构与算法分析/第6章 优先队列(堆)/BinHeap.c
@@ -1,8 +1,29 @@
+#include <stdlib.h>
 #include "BinHeap.h"
 
+#define MinData -100
 struct HeapStruct
 {
     int Capacity;
     int Size;
     ElementType *Elements;
 };
+
+PriorityQueue Initialize(int MaxElements)
+{
+    PriorityQueue H;
+    H = malloc(sizeof(struct HeapStruct));
+    if (H == NULL)
+    {
+        //error
+    }
+    H->Elements = malloc(sizeof(ElementType) * (MaxElements + 1));
+    if (H->Elements == NULL)
+    {
+        //error
+    }
+    H->Size = 0;
+    H->Capacity = MaxElements;
+    H->Elements[0] = MinData;
+    return H;
+}
