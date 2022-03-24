26e55b30faaf131127f9610a1ade85ec5d2e0379
JX-Zhuang <who.zhuang@gmail.com>
2022-03-24 23:48:07 +0800

    update

--- 数据结构与算法分析/第7章 排序/Quicksort.c
+++ 数据结构与算法分析/第7章 排序/Quicksort.c
@@ -3,3 +3,11 @@ void Quicksort(ElementType A[], int N)
 {
     Qsort(A, 0, N - 1);
 }
+void Qsort(ElementType A[], int Left, int Right)
+{
+    int i, j;
+    ElementType Pivot = A[(Left + Right) / 2];
+
+    Qsort(A, Left, i - 1);
+    Qsort(A, i + 1, Right);
+}
