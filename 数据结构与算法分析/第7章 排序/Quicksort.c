5a7aaa23ad2a84c284463e79fa994a5b13cb8720
JX-Zhuang <who.zhuang@gmail.com>
2022-03-25 19:11:29 +0800

    qsort

--- 数据结构与算法分析/第7章 排序/Quicksort.c
+++ 数据结构与算法分析/第7章 排序/Quicksort.c
@@ -1,13 +1,52 @@
+#include <stdio.h>
 typedef int ElementType;
+void Swap(ElementType *A, ElementType *B)
+{
+    ElementType Tmp = *A;
+    *A = *B;
+    *B = Tmp;
+}
+int Partition(ElementType A[], int Left, int Right)
+{
+    ElementType Pivot = A[Left];
+    int i = Left + 1, j = Right;
+    while (i < j)
+    {
+        while (A[i] <= Pivot && i <= j)
+        {
+            i++;
+        }
+        while (A[j] >= Pivot && i <= j)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            Swap(&A[i], &A[j]);
+        }
+    }
+    Swap(&A[Left], &A[j]);
+    return j;
+}
+void Qsort(ElementType A[], int Left, int Right)
+{
+    if (Left >= Right)
+        return;
+    int i = Partition(A, Left, Right);
+    Qsort(A, Left, i - 1);
+    Qsort(A, i + 1, Right);
+}
 void Quicksort(ElementType A[], int N)
 {
     Qsort(A, 0, N - 1);
 }
-void Qsort(ElementType A[], int Left, int Right)
+int main()
 {
-    int i, j;
-    ElementType Pivot = A[(Left + Right) / 2];
-    
-    Qsort(A, Left, i - 1);
-    Qsort(A, i + 1, Right);
+    ElementType A[] = {6, 7, 3, 2, 5, 6, 8, 10, 4, 9, 1, 6};
+    Quicksort(A, 12);
+    for (int i = 0; i < 12; i++)
+    {
+        printf("%d\n", A[i]);
+    }
+    return 0;
 }
