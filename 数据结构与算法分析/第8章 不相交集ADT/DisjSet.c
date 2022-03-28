e0b9d8ad385b615ebffdb466882ccfe1f5e93157
JX-Zhuang <who.zhuang@gmail.com>
2022-03-28 23:14:14 +0800

    并查集

---
+++ 数据结构与算法分析/第8章 不相交集ADT/DisjSet.c
@@ -0,0 +1,20 @@
+#include "DisjSet.h"
+#define NumSets 10
+void Initilialize(DisjSet S)
+{
+    int i;
+    for (i = NumSets; i > 0; i--)
+        S[i] = 0;
+}
+void SetUnion(DisjSet S, SetType Root1, SetType Root2)
+{
+    S[Root2] = Root1;
+}
+SetType Find(ElementType X, DisjSet S)
+{
+    if (S[X] <= 0)
+    {
+        return X;
+    }
+    return Find(S[X], S);
+}
